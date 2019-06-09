d748aac1f562c6a85fe225811b72a477b7fbbbb3
LucianoRaineriMarchina <luciano.rmna@gmail.com>
2019-06-09 08:44:53 -0300

    fixed server

--- server/src/core/emitter/signal_emitter.py
+++ server/src/core/emitter/signal_emitter.py
@@ -11,22 +11,3 @@ class SignalEmitter:
     @abstractmethod
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @abstractmethod
-    def __init__(self, id: str, **kwargs):
-        self._id = id
-        self.name = kwargs.pop(NAME_KEY, None)
-        super().__init__(**kwargs)
-
-    @property
-    def id(self):
-        return self._id
-
-    def __eq__(self, other: 'Object'):
-        if isinstance(other, self.__class__):
-            return self.id == other.id
-        else:
-            return False
-
-    def __ne__(self, other: 'Object'):
-        return not self.__eq__(other)
